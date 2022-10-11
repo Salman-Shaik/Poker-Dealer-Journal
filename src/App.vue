ac7cacaa4cf01943ef0bf2c3009d42825cbefcc2
Salman-Shaik <dukesalman5@gmail.com>
2022-10-11 14:02:09 +0530

    Added First Oage

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+import PlayersInfo from "./components/PlayersInfo.vue";
+
+export default {
+  name: "app",
+  components: {
+    PlayersInfo,
+  },
+};
+</script>
+
+<template>
+  <header>
+    <h1>Poker Dealer Journal</h1>
+  </header>
+
+  <main>
+    <players-info></players-info>
+    <section class="journal"></section>
+    <section class="stats"></section>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/PlayersInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="players-info">
+    <h4>Players</h4>
+    <section class="players-input">
+      <input
+        type="text"
+        placeholder="Player Name"
+        v-model="nameInput"
+        @change="onChange"
+        autofocus
+      />
+      <button @click="addPlayer">Enter</button>
+    </section>
+    <section class="players" v-for="player in players">
+      <span>
+        <h5>{{ player }}</h5>
+        <button @click="removePlayer(player)">X</button>
+      </span>
+    </section>
+    <button>Start</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      players: [],
+      nameInput: "",
+    };
+  },
+  methods: {
+    onChange(event) {
+      const playerName = event.target.value;
+      this.nameInput = playerName;
+    },
+    addPlayer() {
+      this.players.push(this.nameInput);
+      this.nameInput = "";
+    },
+    removePlayer(player) {
+      const playerIndex = this.players.indexOf(player);
+      this.players.splice(playerIndex, 1);
+    },
+  },
+};
+</script>
